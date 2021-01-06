704881998d8bc9ef491974d473e685f473f2ec07
苏家玉 <906094945@qq.com>
2021-01-06 17:09:46 +0800

    null

---
+++ ToolInit.js
@@ -0,0 +1,19 @@
+var Tool={}
+var Data={}
+var Config={}
+
+window.Tool=Tool;
+window.Data=Data;
+window.Config;
+
+//Tool.gui=require("");
+//Tool.event=require("");
+Tool.sound=require("");
+Tool.ani=require("");
+Tool.effect=require("");
+Tool.http=require("");
+
+
+Data.aaa=require("DataInit");
+Config=require("Config");
+
